a3e8dce5851fc9d4aa1d55ab4ef38d469c8139d4
Cynzy <cynthiazhou98@gmail.com>
2017-11-26 21:03:27 -0800

    Added ANTLR grammar

--- src/main/java/ca/ece/ubc/cpen221/mp5/Query/MP5QueryLexer.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/Query/MP5QueryLexer.java
@@ -1,12 +1,11 @@
 // Generated from MP5Query.g4 by ANTLR 4.7
-import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenStream;
+package ca.ece.ubc.cpen221.mp5.Query;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.atn.ATN;
+import org.antlr.v4.runtime.atn.ATNDeserializer;
+import org.antlr.v4.runtime.atn.LexerATNSimulator;
+import org.antlr.v4.runtime.atn.PredictionContextCache;
 import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class MP5QueryLexer extends Lexer {
@@ -16,8 +15,8 @@ public class MP5QueryLexer extends Lexer {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		OR=1, AND=2, GT=3, GTE=4, LT=5, LTE=6, EQ=7, NUM=8, LPAREN=9, RPAREN=10, 
-		IN=11, CATEGORY=12, NAME=13, RATING=14, PRICE=15;
+		AND=1, OR=2, LPAREN=3, RPAREN=4, GT=5, GTE=6, LT=7, LTE=8, EQ=9, NUM=10, 
+		IN=11, CATEGORY=12, NAME=13, RATING=14, PRICE=15, STR=16;
 	public static String[] channelNames = {
 		"DEFAULT_TOKEN_CHANNEL", "HIDDEN"
 	};
@@ -27,17 +26,17 @@ public class MP5QueryLexer extends Lexer {
 	};
 
 	public static final String[] ruleNames = {
-		"OR", "AND", "GT", "GTE", "LT", "LTE", "EQ", "NUM", "LPAREN", "RPAREN", 
-		"IN", "CATEGORY", "NAME", "RATING", "PRICE"
+		"AND", "OR", "LPAREN", "RPAREN", "GT", "GTE", "LT", "LTE", "EQ", "NUM", 
+		"IN", "CATEGORY", "NAME", "RATING", "PRICE", "STR"
 	};
 
 	private static final String[] _LITERAL_NAMES = {
-		null, "'||'", "'&&'", "'>'", "'>='", "'<'", "'<='", "'='", null, "'('", 
-		"')'", "'in'", "'category'", "'name'", "'rating'", "'price'"
+		null, "'&&'", "'||'", "'('", "')'", "'>'", "'>='", "'<'", "'<='", "'='", 
+		null, "'in'", "'category'", "'name'", "'rating'", "'price'"
 	};
 	private static final String[] _SYMBOLIC_NAMES = {
-		null, "OR", "AND", "GT", "GTE", "LT", "LTE", "EQ", "NUM", "LPAREN", "RPAREN", 
-		"IN", "CATEGORY", "NAME", "RATING", "PRICE"
+		null, "AND", "OR", "LPAREN", "RPAREN", "GT", "GTE", "LT", "LTE", "EQ", 
+		"NUM", "IN", "CATEGORY", "NAME", "RATING", "PRICE", "STR"
 	};
 	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
 
@@ -97,27 +96,29 @@ public class MP5QueryLexer extends Lexer {
 	public ATN getATN() { return _ATN; }
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\21W\b\1\4\2\t\2\4"+
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\22^\b\1\4\2\t\2\4"+
 		"\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t"+
-		"\13\4\f\t\f\4\r\t\r\4\16\t\16\4\17\t\17\4\20\t\20\3\2\3\2\3\2\3\3\3\3"+
-		"\3\3\3\4\3\4\3\5\3\5\3\5\3\6\3\6\3\7\3\7\3\7\3\b\3\b\3\t\3\t\3\n\3\n\3"+
-		"\13\3\13\3\f\3\f\3\f\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\16\3\16\3\16"+
-		"\3\16\3\16\3\17\3\17\3\17\3\17\3\17\3\17\3\17\3\20\3\20\3\20\3\20\3\20"+
-		"\3\20\2\2\21\3\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25\f\27\r\31\16"+
-		"\33\17\35\20\37\21\3\2\3\3\2\63\67\2V\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2"+
-		"\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2"+
-		"\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3"+
-		"\2\2\2\2\37\3\2\2\2\3!\3\2\2\2\5$\3\2\2\2\7\'\3\2\2\2\t)\3\2\2\2\13,\3"+
-		"\2\2\2\r.\3\2\2\2\17\61\3\2\2\2\21\63\3\2\2\2\23\65\3\2\2\2\25\67\3\2"+
-		"\2\2\279\3\2\2\2\31<\3\2\2\2\33E\3\2\2\2\35J\3\2\2\2\37Q\3\2\2\2!\"\7"+
-		"~\2\2\"#\7~\2\2#\4\3\2\2\2$%\7(\2\2%&\7(\2\2&\6\3\2\2\2\'(\7@\2\2(\b\3"+
-		"\2\2\2)*\7@\2\2*+\7?\2\2+\n\3\2\2\2,-\7>\2\2-\f\3\2\2\2./\7>\2\2/\60\7"+
-		"?\2\2\60\16\3\2\2\2\61\62\7?\2\2\62\20\3\2\2\2\63\64\t\2\2\2\64\22\3\2"+
-		"\2\2\65\66\7*\2\2\66\24\3\2\2\2\678\7+\2\28\26\3\2\2\29:\7k\2\2:;\7p\2"+
-		"\2;\30\3\2\2\2<=\7e\2\2=>\7c\2\2>?\7v\2\2?@\7g\2\2@A\7i\2\2AB\7q\2\2B"+
-		"C\7t\2\2CD\7{\2\2D\32\3\2\2\2EF\7p\2\2FG\7c\2\2GH\7o\2\2HI\7g\2\2I\34"+
-		"\3\2\2\2JK\7t\2\2KL\7c\2\2LM\7v\2\2MN\7k\2\2NO\7p\2\2OP\7i\2\2P\36\3\2"+
-		"\2\2QR\7r\2\2RS\7t\2\2ST\7k\2\2TU\7e\2\2UV\7g\2\2V \3\2\2\2\3\2\2";
+		"\13\4\f\t\f\4\r\t\r\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\3\2\3\2\3"+
+		"\2\3\3\3\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\7\3\b\3\b\3\t\3\t\3\t"+
+		"\3\n\3\n\3\13\3\13\3\f\3\f\3\f\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\16"+
+		"\3\16\3\16\3\16\3\16\3\17\3\17\3\17\3\17\3\17\3\17\3\17\3\20\3\20\3\20"+
+		"\3\20\3\20\3\20\3\21\6\21[\n\21\r\21\16\21\\\2\2\22\3\3\5\4\7\5\t\6\13"+
+		"\7\r\b\17\t\21\n\23\13\25\f\27\r\31\16\33\17\35\20\37\21!\22\3\2\4\3\2"+
+		"\63\67\4\2C\\c|\2^\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13"+
+		"\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2"+
+		"\2\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2"+
+		"!\3\2\2\2\3#\3\2\2\2\5&\3\2\2\2\7)\3\2\2\2\t+\3\2\2\2\13-\3\2\2\2\r/\3"+
+		"\2\2\2\17\62\3\2\2\2\21\64\3\2\2\2\23\67\3\2\2\2\259\3\2\2\2\27;\3\2\2"+
+		"\2\31>\3\2\2\2\33G\3\2\2\2\35L\3\2\2\2\37S\3\2\2\2!Z\3\2\2\2#$\7(\2\2"+
+		"$%\7(\2\2%\4\3\2\2\2&\'\7~\2\2\'(\7~\2\2(\6\3\2\2\2)*\7*\2\2*\b\3\2\2"+
+		"\2+,\7+\2\2,\n\3\2\2\2-.\7@\2\2.\f\3\2\2\2/\60\7@\2\2\60\61\7?\2\2\61"+
+		"\16\3\2\2\2\62\63\7>\2\2\63\20\3\2\2\2\64\65\7>\2\2\65\66\7?\2\2\66\22"+
+		"\3\2\2\2\678\7?\2\28\24\3\2\2\29:\t\2\2\2:\26\3\2\2\2;<\7k\2\2<=\7p\2"+
+		"\2=\30\3\2\2\2>?\7e\2\2?@\7c\2\2@A\7v\2\2AB\7g\2\2BC\7i\2\2CD\7q\2\2D"+
+		"E\7t\2\2EF\7{\2\2F\32\3\2\2\2GH\7p\2\2HI\7c\2\2IJ\7o\2\2JK\7g\2\2K\34"+
+		"\3\2\2\2LM\7t\2\2MN\7c\2\2NO\7v\2\2OP\7k\2\2PQ\7p\2\2QR\7i\2\2R\36\3\2"+
+		"\2\2ST\7r\2\2TU\7t\2\2UV\7k\2\2VW\7e\2\2WX\7g\2\2X \3\2\2\2Y[\t\3\2\2"+
+		"ZY\3\2\2\2[\\\3\2\2\2\\Z\3\2\2\2\\]\3\2\2\2]\"\3\2\2\2\4\2\\\2";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
